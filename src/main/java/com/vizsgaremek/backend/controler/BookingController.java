bc9186bcb2dbbbf6df8be737c880f558ce9f701f
mmartin027 <malajmartin3@gmail.com>
2026-01-05 12:09:24 +0100

    dto

--- src/main/java/com/vizsgaremek/backend/controler/BookingController.java
+++ src/main/java/com/vizsgaremek/backend/controler/BookingController.java
@@ -1,10 +1,34 @@
 package com.vizsgaremek.backend.controler;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vizsgaremek.backend.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+
+
 public class BookingController {
+
+
+
+    private final BookingService bookingService;
+
+    /**
+     * Új foglalás létrehozása
+     */
+    @PostMapping
+    public ResponseEntity<BookingResponse> createBooking(@Valid @RequestBody BookingRequest request) {
+        BookingResponse booking = bookingService.createBooking(request);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(booking);
+    }
+
 
 }
---
+++ src/main/java/com/vizsgaremek/backend/service/BookingService.java
@@ -0,0 +1,4 @@
+package com.vizsgaremek.backend.service;
+
+public class BookingService {
+}
